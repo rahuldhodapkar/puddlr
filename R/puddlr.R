db26fa954fb717c9e30d8942d30f390c85007c96
Rahul Dhodapkar <rahuldhodapkar@Rahuls-MBP.lan>
2022-03-28 10:34:16 -0400

    update with glm MVP

--- R/puddlr.R
+++ R/puddlr.R
@@ -14,6 +14,78 @@
 #     You should have received a copy of the GNU Affero General Public License
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+################################################################################
+##
+################################################################################
+
+#' forked from rsq package.
+#' Zhang (2017): variance-function-based R-squared.
+#'
+#' @param fitObj glm or lm object to calculate R-squared for
+#' @param adj whether to adjust R-squared value.
+#'
+#' @return A normalized puddlr object
+#'
+#' @importFrom rsq vresidual
+#'
+#' @rdname rsq.v
+#' @export rsq.v
+#'
+rsq.v <- function(fitObj,adj=FALSE)
+{
+    y <- fitObj$y
+    wt <- weights(fitObj)
+    if( is.null(wt) )
+      wt <- y*0+1
+
+    n <- sum(wt)
+    if(is.null(y)) stop("The glm object does not include response values!")
+    yfit <- fitObj$fitted.values
+
+    if(pmatch("Negative Binomial",family(fitObj)$family,nomatch=F))
+    {
+      theta <- ifelse(is.null(fitObj$theta),
+                      as.numeric(gsub("(?<=\\()[^()]*(?=\\))(*SKIP)(*F)|.","",
+                                      family(fitObj)$family, perl=T)), fitObj$theta)
+      sse1 <- sum(wt*vresidual(y,yfit,family=negative.binomial(theta))^2)
+      
+      #y <- model.response(fitObj$model)
+      f0 <- glm(y~1,family=negative.binomial(theta))
+      yf0 <- f0$fitted.values
+      sse0 <- sum(wt*vresidual(y,yf0,family=negative.binomial(theta))^2)
+    }
+    else if(family(fitObj)$family=="binomial")
+    {
+      nSuc <- wt*y
+      nFai <- wt-nSuc
+      tone <- rep(1,length(nSuc))
+      sse1 <- sum(nSuc*vresidual(tone,yfit,family=family(fitObj))^2)+
+        sum(nFai*vresidual(1-tone,yfit,family=family(fitObj))^2)
+      
+      #f0 <- update(fitObj,.~1,data=data)
+      f0 <- update(fitObj,.~1, family=fitObj$family, data=fitObj$data)
+      yf0 <- f0$fitted.values
+      sse0 <- sum(nSuc*vresidual(tone,yf0,family=family(f0))^2)+
+                sum(nFai*vresidual(1-tone,yf0,family=family(f0))^2)
+    }
+    else
+    {
+      sse1 <- sum(wt*vresidual(y,yfit,family=family(fitObj))^2)
+      
+      f0 <- update(fitObj,.~1)
+      sse0 <- sum(wt*vresidual(y,f0$fitted.values,family=family(f0))^2)
+    }
+
+    #rsq <- 1-(sse1/sse0)*ifelse(adj,f0$df.residual/fitObj$df.residual,1)
+    pM <- fitObj$df.null-fitObj$df.residual+1
+    rsq <- 1-(sse1/sse0)*ifelse(adj,(n-1)/(n-pM),1)
+
+    rsq
+}
+
+################################################################################
+##
+################################################################################
 
 #' Create Puddlr object
 #'
@@ -50,8 +122,6 @@ CreatePuddlrObject <- function(response, predictors) {
 #'
 #' @return A normalized puddlr object
 #'
-#' @importFrom stats na.omit
-#'
 #' @rdname NormalizePredictors
 #' @export NormalizePredictors
 #'
@@ -84,20 +154,113 @@ NormalizePredictors <- function(puddlr,
     return(puddlr)
 }
 
+#'
+#' Runs PCA for linear dimensionality reduction on the predictors of a 
+#' normalized puddlr object
+#'
+#' @param puddlr    A puddlr object
+#'
+#' @return puddlr object with pca reduction
+#'
+#' @importFrom stats prcomp
+#'
+#' @rdname RunPCA
+#' @export RunPCA
+#'
+RunPCA <- function(puddlr) {
+    pca <- prcomp(x=puddlr$predictors)
+
+    puddlr$reductions <- list()
+    puddlr$reductions$pca <- list()
+
+    puddlr$reductions$pca$embedding <- pca$x
+    puddlr$reductions$pca$rotation <- pca$rotation
+    puddlr$reductions$pca$sdev <- pca$sdev
+
+    return(puddlr)
+}
+
+#' Runs generalized linear model on a puddlr object.  Intended to be run after
+#' normalization and removal of incomplete observations.
+#'
+#' @param puddlr    A puddlr object
+#' @param formula   an object of class "formula" (or one that can be coerced to
+#'                  that class): a symbolic description of the model to be 
+#'                  fitted. The details of model specification can be found 
+#'                  under `glm`.  The name of the response variable in the
+#'                  formula will be used to name the response variable.
+#' @param family    a description of the error distribution and link function
+#'                  to be used in the model.  Passed to the argument of the
+#'                  same name under `glm`.
+#' @param reduction a string specifying the linear dimensionality reduction to
+#'                  use. Valid options are \code{'pca'}.  Default = \code{'pca'}
+#' @param n.components an integer specifying the total number of reduction
+#'                  components to use in the GLM.
+#' @param adj.rsq   boolean flag specifying whether to adjust the pseudo-R^2
+#'                  goodness-of-fit calculated value.
+#'
+#' @return puddlr object with glm
+#'
+#' @rdname RunGLM
+#' @export RunGLM
+#'
+RunGLM <- function(puddlr,
+                   formula,
+                   family,
+                   reduction,
+                   n.components,
+                   adj.rsq=FALSE) {
+
+    response.var.name <- all.vars(formula)[1]
+
+    data.df <- data.frame(puddlr$reductions[[reduction]]$embedding[,1:n.components])
+    data.df[,response.var.name] <- as.factor(puddlr$response)
+
+    puddlr$model <- list()
+
+    puddlr$model$obj <- glm(
+      formula = as.formula(paste0(response.var.name, '~.')),
+      family = family,
+      data = data.df
+    )
+    # *** TODO *** bug with rsq package
+    puddlr$model$rsq <- rsq.v(puddlr$model$obj, adj=adj.rsq)
+    #puddlr$model$rsq <- pR2(puddlr$model$obj)['McFadden']
+
+    # project back to original feature space
+    sum.feature.estimate <- (
+        rowSums(t(
+            t(puddlr$reductions[[reduction]]$rotation[,1:n.components])
+            * puddlr$model$obj$coefficients[(1:n.components)+1]
+        ))
+    )
+
+    sum.feature.var <- c()
+    for (i in 1:ncol(puddlr$predictors)) {
+        vconv.unscaled <- vcov(puddlr$model$obj)[
+                                1:n.components+1,
+                                1:n.components+1]
+        v <- puddlr$reductions[[reduction]]$rotation[i,1:n.components]
+        var.i <- sum(t(vconv.unscaled * v) * v)
+        sum.feature.var <- c(sum.feature.var, var.i)
+    }
+    names(sum.feature.var) <- colnames(puddlr$predictors)
+
+    stats.df <- data.frame(
+        VariableName = names(sum.feature.var),
+        GLMEstimate = sum.feature.estimate[ names(sum.feature.var) ],
+        StdErr = sqrt( sum.feature.var )
+    )
+    stats.df$ZScore <- stats.df$GLMEstimate / stats.df$StdErr
+    stats.df$PValue <- 2*pnorm(-abs(stats.df$ZScore))
+
+    puddlr$model$df <- stats.df
+
+    return(puddlr)
+}
+
 ################################################################################
 ## Methods
 ################################################################################
 
-#' Pretty print details of puddlr object
-#'
-#' @param puddlr A puddlr object
-#'
-#' @return NULL
-#'
-#' @rdname print.puddlr
-#' @export print.puddlr
-#'
-print.puddlr <- function(puddlr) {
-    print("A Puddlr Object")
-}
-
+# not yet written
---
+++ tests/testthat/test_glm.R
@@ -0,0 +1,74 @@
+# test_glm.R
+#     Copyright (C) 2022  Rahul Dhodapkar
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+context("Test GLM")
+library(puddlr)
+library(stringr)
+
+test_that("Test PCA", {
+    response.test <- as.factor(c(1,0,1,1))
+    predictors.test <- matrix(c( 1, 2, 3, 4, 
+                                 4, 3, 2, 1, 
+                                 0, 3, 1, 0, 
+                                 0.5, 1, 2, 1,
+                                 5, 6, 5, 1,
+                                 10, 20, 30, 40),
+                              nrow=4, ncol=6)
+
+    rownames(predictors.test) <- paste0('r', 1:nrow(predictors.test))
+    colnames(predictors.test) <- paste0('c', 1:ncol(predictors.test))
+
+    puddlr.obj <- CreatePuddlrObject(
+        response = response.test,
+        predictors = predictors.test
+    )
+    puddlr.obj <- NormalizePredictors(puddlr.obj)
+    puddlr.obj <- RunPCA(puddlr.obj)
+    puddlr.obj <- RunGLM(puddlr.obj, 
+                         formula=response ~ .,
+                         family=binomial(link='logit'),
+                         reduction='pca',
+                         n.components=3)
+
+    expect_equal(class(puddlr.obj), 'puddlr')
+})
+
+test_that("Test PCA", {
+    set.seed(42)
+
+    response.test <- sample(c(0,1), size=100, replace=TRUE)
+    predictors.test <- matrix(runif(100 * 1000),
+                              nrow=100, ncol=1000)
+
+    rownames(predictors.test) <- paste0('r', 1:nrow(predictors.test))
+    colnames(predictors.test) <- paste0('c', 1:ncol(predictors.test))
+
+    puddlr.obj <- CreatePuddlrObject(
+        response = response.test,
+        predictors = predictors.test
+    )
+    puddlr.obj <- NormalizePredictors(puddlr.obj)
+    puddlr.obj <- RunPCA(puddlr.obj)
+    puddlr.obj <- RunGLM(puddlr.obj, 
+                         formula=y ~ .,
+                         family=binomial(link='logit'),
+                         reduction='pca',
+                         n.components=3,
+                         adj.rsq=FALSE)
+
+    expect_equal(class(puddlr.obj), 'puddlr')
+})
